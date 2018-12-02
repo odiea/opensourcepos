11579b0adc9ff0801f98eb473d278898b231f4d8
odiea <oagnew@aim.com>
2018-12-02 09:42:05 -0700

    Add quick cash tables

---
+++ database/Quick_cash.sql
@@ -0,0 +1,11 @@
+INSERT INTO `ospos_app_config` (`key`, `value`) VALUES
+('quick_cash_enable', '1'),
+('quick_cash_1', '1'),
+('quick_cash_2', '5'),
+('quick_cash_3', '10'),
+('quick_cash_4', '20'),
+('quick_cash_5', '50'),
+('quick_cash_6', '100');
+
+
+
