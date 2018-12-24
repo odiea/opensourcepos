77cd24f5b4c0730f122ed3df1117e6d5295acf4d
odiea <oagnew@aim.com>
2018-12-24 13:07:40 -0700

    Misc changes remove home and office icons

--- application/views/home/office.php
+++ application/views/home/office.php
@@ -6,17 +6,6 @@
 
 <h3 class="text-center"><?php echo $this->lang->line('common_welcome_message'); ?></h3>
 
-<div id="office_module_list" class="text-center">
-	<?php
-	foreach($allowed_modules as $module)
-	{
-	?>
-		<div class="module_item" title="<?php echo $this->lang->line('module_'.$module->module_id.'_desc');?>">
-			<a class="btn btn-primary" href="<?php echo site_url("$module->module_id");?>"><?php echo $this->lang->line("module_".$module->module_id) ?></a>
-		</div>
-	<?php
-	}
-	?>
-</div>
+
 
 <?php $this->load->view("partial/footer"); ?>
